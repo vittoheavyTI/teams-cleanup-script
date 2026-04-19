e03845fd222866c0849d426c14c50b3a81a5dc5e
vittoheavyTI <vittoheavymetal@gmail.com>
2026-04-19 04:53:23 -0300

    Create Clean-MSTeams.ps1

---
+++ Clean-MSTeams.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Deep Clean Microsoft Teams (Classic & New)
+    .DESCRIPTION
+        Este script realiza uma limpeza completa do Microsoft Teams, removendo tanto a versao Classica 
+            quanto a versao Nova (Work or School), excluindo arquivos locais, chaves de registro e desinstalando 
+                os componentes do sistema.
+                .AUTHOR
+                    VittoheavyTI
+                    .LICENSE
+                        MIT
+                        #>
+                        
